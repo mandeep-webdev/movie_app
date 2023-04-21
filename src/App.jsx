40d6faa7d03240cb4d7b53463d24e2b0a9c0d347
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2023-04-21 15:09:46 +0530

    done with the cofiguration of redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import { useEffect } from 'react';
 import { fetchDataFromApi } from './utils/app';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { getApiConfiguration, getGenres } from './store/slice';
 
 function App() {
+  const { url } = useSelector((state) => state.home);
+  const dispatch = useDispatch();
+
   useEffect(() => {
     fetch();
   }, []);
 
   const fetch = async () => {
-    const res = await fetchDataFromApi('/movie/latest');
+    const res = await fetchDataFromApi('/movie/popular');
+    dispatch(getApiConfiguration(res));
     console.log(res);
   };
 
   return (
     <>
-      <div className="App">App</div>
+      <div className="App">App{url?.total_pages}</div>
     </>
   );
 }
