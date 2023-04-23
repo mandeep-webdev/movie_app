a318abc0313192feb60db487602d930135c5de7e
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2023-04-23 15:25:20 +0530

    done the fuctionality of switch buttons

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import './styles.scss';
 import HeroBanner from './heroBanner/HeroBanner';
-
+import Trending from './trending/Trending';
 const Home = () => {
   return (
     <div>
       <HeroBanner />
+      <Trending />
       <div style={{ height: '1000px' }}></div>
     </div>
   );
