dfdebbb317deec7a8b90f1c04885159cde0bb8b0
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2023-04-24 14:13:46 +0530

    added whats popular carousel component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import './styles.scss';
 import HeroBanner from './heroBanner/HeroBanner';
 import TrendingMovies from './trending/TrendingMovies';
 import TrendingTv from './trending/TrendingTv';
+import Popular from './popular/Popular';
 const Home = () => {
   return (
     <div>
       <HeroBanner />
       <TrendingMovies />
       <TrendingTv />
+      <Popular />
       <div style={{ height: '1000px' }}></div>
     </div>
   );
